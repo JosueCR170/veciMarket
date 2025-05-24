db2428be0658960a4c90a633f4d11d0921c4e6e8
Daniel <danielgonzalezpicad@gmail.com>
2025-05-24 02:43:49 -0600

    Producto

--- front-market/src/pages/Agregar.tsx
+++ front-market/src/pages/Agregar.tsx
@@ -1,18 +1,19 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import Navbar from '../components/navbar/navbar';
 import './Tab2.css';
+import AgregarProducto from '../components/agregarProducto/agregarProducto';
 
 const Agregar: React.FC = () => {
   return (
     <IonPage>
-      <Navbar/>
+      <Navbar />
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Agregar</IonTitle>
           </IonToolbar>
         </IonHeader>
-     
+        <AgregarProducto />
       </IonContent>
     </IonPage>
   );
