63b3fa4f106d8857ffe841aca8fa8e419943c0c9
Daniel <danielgonzalezpicad@gmail.com>
2025-06-06 17:59:39 -0600

    Commit push de modal producto y card

--- front-market/src/components/cardProducto/cardProducto.tsx
+++ front-market/src/components/cardProducto/cardProducto.tsx
@@ -5,6 +5,7 @@ import {
   IonCardHeader,
   IonCardTitle,
   IonImg,
+  IonText,
 } from '@ionic/react';
 import './cardProducto.css';
 
@@ -17,7 +18,7 @@ interface Producto {
   precio: number;
   categoria: string;
   idVendedor?: string;
-  contacto: React.ReactNode; 
+  contacto: React.ReactNode;
 }
 
 interface CardProductoProps {
@@ -27,15 +28,22 @@ interface CardProductoProps {
 
 const CardProducto: React.FC<CardProductoProps> = ({ producto, onClick }) => {
   return (
-    <IonCard onClick={() => onClick(producto)} style={{ cursor: 'pointer' }}>
-      <IonImg src={producto.img} alt={producto.nombre} />
-      <IonCardHeader>
-        <IonCardTitle>{producto.nombre}</IonCardTitle>
-      </IonCardHeader>
-      <IonCardContent>
-        <p>${producto.precio}</p>
-        <p>{producto.descripcion}</p>
-      </IonCardContent>
+    <IonCard className="product-card" onClick={() => onClick(producto)}>
+      <div className="text-container">
+        <div className="image-wrapper">
+          <IonImg className="imgProducto" src={producto.img} />
+        </div>
+        <div className="text-content">
+          <IonCardHeader>
+            <IonText className="product-name">{producto.nombre}</IonText>
+          </IonCardHeader>
+          <IonCardContent>
+            <IonText className="product-info">
+              ${producto.precio} - {producto.categoria}
+            </IonText>
+          </IonCardContent>
+        </div>
+      </div>
     </IonCard>
   );
 };
